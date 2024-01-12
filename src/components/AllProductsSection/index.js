46a6b28b13ab99c27e2fe3600bf27f352a102782
root <rajkumaraina3@gmail.comgit config --global user.email rajkumaraina3@gmail.comgit config --global user.name "rajkumaraina>
2024-01-12 12:09:04 +0000

    NxtTrendz Authentication and Authorization

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,68 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+    isLoading: true,
+  }
+
+  componentDidMount = () => {
+    this.getProductDetails()
+  }
+
+  getProductDetails = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      const updatedData = data.products.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({productsList: updatedData, isLoading: false})
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList, isLoading} = this.state
+    return isLoading ? (
+      <div className="spinner">
+        <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+      </div>
+    ) : (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
